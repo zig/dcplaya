d61f65cae67ed5c50d6fc5204631f07f0fba884e
ben <ben>
2002-09-11 03:29:51 +0000

    Fixed a bug when substitutes by empty string.

--- utils/shasubst.sh
+++ utils/shasubst.sh
@@ -8,23 +8,23 @@ line=""
 function transform
 {
     name="`expr "$1" : '\(.*\)=.*' \| "$1"`"
-    val="`expr "$1" : '.*=\(.*\)' \| "$1"`"
-    echo "name:[$name]" >&2
-    echo "val:[$val]" >&2
+    val="`expr "$1" : '.*=\(.*\)' \| ""`"
+#    echo "name:[$name]" >&2
+#    echo "val:[$val]" >&2
     echo "s#\\\$${name}\\\$#${val}#"
 }
 
 
-# function tam
-# {
-#     i=1
-#     echo "NUMBER: $#" >&2
-#     while [ $# -ne 0 ] ; do
-# 	echo $i "[$1]" [$2] >&2
-# 	shift 2
-# 	i=`expr $i + 1`
-#     done
-# }
+function tam
+{
+    i=1
+    echo "NUMBER: $#" >&2
+    while [ $# -ne 0 ] ; do
+		echo $i "[$1]" [$2] >&2
+		shift 2
+		i=`expr $i + 1`
+    done
+}
 
 i=0;
 while [ $# -ne 0 ]; do
