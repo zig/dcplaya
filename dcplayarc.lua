c0eac02cb91827187b463a2061d9cc795f01aca8
zig <zig>
2002-09-14 01:11:45 +0000

    config file

--- dcplayarc.lua
+++ dcplayarc.lua
@@ -1,9 +1,32 @@
-print ("Welcome to Dcplaya !\n")
+-- This is main DCplaya lua script
+
+print ("Welcome to DCplaya !\n")
+
+print ("Home is set to '", home, "'\n")
+
+-- print available commands
+help()
+print("")
 
 
-plug_spc = home.."plugins/inp/spc/spc.lef"
+-- reading directory on PC is slow through serial port, 
+-- so we precalculate available plugins instead of doing a dir_load command
+plug_spc	= home.."plugins/inp/spc/spc.lef"
+plug_xing	= home.."plugins/inp/xing/xing.lef"
+plug_sidplay	= home.."plugins/inp/sidplay/sidplay.lef"
+plug_obj	= home.."plugins/obj/obj.lef"
+plug_lpo	= home.."plugins/vis/lpo/lpo.lef"
+plug_fftvlr	= home.."plugins/vis/fftvlr/fftvlr.lef"
 
 
 
-dl(plug_spc)
 
+
+-- reading user config
+print ("Reading user config file 'userconf.lua'")
+dofile (home.."userconf.lua")
+
+
+-- Exemple of command to put into userconf.lua :
+--
+--    driver_load(plug_spc)
