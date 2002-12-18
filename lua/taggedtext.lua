73a4d5e720e66511f69f15baad3d149ae181a9e3
zigziggy <zigziggy>
2002-12-18 20:47:03 +0000

    *** empty log message ***

--- lua/taggedtext.lua
+++ lua/taggedtext.lua
@@ -17,12 +17,13 @@ end
 
 function tt_img_cmd(mode, param)
    local src = param.src
-   if not src then
-      return
-   end
    local name = param.name or src
    local spr = sprite_get(name)
    if not spr then
+      if not src then
+	 return
+      end
+
       --local tex = tex_new()
       --print("'"..src.."'")
       local tex = tex_get(home.."lua/rsc/icons/"..src) or tex_new(home.."lua/rsc/icons/"..src)
@@ -51,14 +52,96 @@ function tt_img_cmd(mode, param)
    end
 end
 
-function tt_br_command(mode)
-   tt_endline(mode)
+
+-- UGLY !!!
+function tt_tocolor(s)
+   if strsub(s, 1, 1) == "#" then
+      local v = 0
+      local i
+      local l = strlen(s)
+      local n = 0
+      local j = 2
+      local col = { 1, 1, 1, 1 }
+      
+      for i=2, l, 1 do
+	 local c
+	 c = strbyte(s, i)
+	 if c >= 97 then
+	    c = c - 97 + 10
+	 elseif c >= 65 then
+	    c = c - 65 + 10
+	 else
+	    c = c - 48
+	 end
+	 v = v * 16 + c
+
+	 n = n+1
+	 if n == 2 then
+	    col[j] = v/255
+	    v = 0
+	    j = j+1
+	    n = 0
+	    if j == 5 then
+	       j=1
+	    end
+	 end
+      end
+
+      return col
+   else
+      local a = tonumber(s) / 255
+      return { 1, a, a, a }
+   end
 end
 
+
+function tt_font_cmd(mode, param)
+
+   mode.font_h = tonumber(param.size) or mode.font_h
+   if param.color then
+--      print("color", param.color)
+      mode.color = tt_tocolor(param.color)
+--      print(mode.color[1], mode.color[2], mode.color[3], mode.color[4])
+   end
+   mode.font_id = param.id or mode.font_id
+--   print(param.size)
+
+   local block = {
+      type = "font_style",
+      dl = mode.dl,
+      id = mode.font_id,
+      h = mode.font_h,
+      w = 0,
+      h = 0,
+      draw = function(block)
+--		print(block.dl, block.id, block.h)
+		dl_text_prop(block.dl, block.id, block.h)
+	     end
+   }
+
+   return block
+end
+
+-- all commands
 tt_commands = {
    img = tt_img_cmd,
-   br = tt_br_command
+   br = function(mode)
+	   tt_endline(mode)
+	end,
+   left = function(mode)
+	     mode.align_line_h = tt_align_line_left
+	  end,
+   center = function(mode)
+	       mode.align_line_h = tt_align_line_center
+	    end,
+   right = function(mode)
+	      mode.align_line_h = tt_align_line_right
+	   end,
+   font = tt_font_cmd
 }
+
+
+
 
 function tt_getword(text, e1)
    if not e1 then
@@ -72,7 +155,7 @@ function tt_getword(text, e1)
    if e1 then
       local e2 = strfind(text, "%s", e1)
       if e2 then
-	 word = strsub(text, e1, e2-1)
+	 word = strsub(text, e1-1, e2-1)
       else
 	 word = strsub(text, e1, -1)
       end
@@ -204,13 +287,11 @@ function tt_build(text, mode)
    mode.bw = mode.box[3] - mode.box[1] -- box width
    mode.w = mode.w or 0
    mode.h = mode.h or 0
-   mode.align_line_h = mode.align_line or tt_align_line_down
-   mode.align_line_v = mode.align_line or tt_align_line_left
+   mode.align_line_v = mode.align_line or tt_align_line_down
+   mode.align_line_h = mode.align_line or tt_align_line_left
 
    local blocks = { n = 0 }
    
-   -- split the text into portions that fits into the width of the box
-   -- TODO handle \n character
    local start = 1
    local len = strlen(text)
    
@@ -220,9 +301,9 @@ function tt_build(text, mode)
 	 e2 = len
       end
 
-      if e1 then
-	 print("sub", strsub(text, start, e2))
-      end
+--      if e1 then
+--	 print("sub", strsub(text, start, e2))
+--      end
 
       local e3, e4
       if start <= e2 then
@@ -248,7 +329,7 @@ function tt_build(text, mode)
 	    color = mode.color,
 	    
 	 }
-	 print("text", block.text)
+--	 print("text", block.text)
 	 block.w, block.h = dl_measure_text(block.dl, block.text, block.font_id, block.font_h, block.font_aspect)
 
 	 tt_insert_block(mode, block)
@@ -261,12 +342,13 @@ function tt_build(text, mode)
 	 --print("tag", tag)
 	 e2 = e4
 
-	 local i = 1, command
+	 local i = 1
+	 local command
 	 command, i = tt_getword(tag, i)
 	 if command then
 	    local param = { }
 	    local key, value
-	    --print("command", command)
+--	    print("command", "'"..command.."'")
 	    key, value, i = tt_getparam(tag, i)
 	    while key do
 --	       if value then
@@ -309,9 +391,9 @@ end
 
 tt = tt_build(
 	      [[
-Hello <img src="vmu"> World ! <br>
-! <br>
-<img src="dcplaya.tga"> TITI <img src="colorpicker.tga"> TOTO
+Hello <img name="vmu"> World ! <br>
+<center> <font size="24" color="#00ffff"> Centered !! <br>
+<right> <font size="48" color="#ffff00"> <img name="dcplaya" src="dcplaya.tga"> TITI <img name="colorpicker" src="colorpicker.tga"> TOTO
 ]], 
 	      { 
 		 box = { 100, 100, 600, 200 },
@@ -320,5 +402,5 @@ Hello <img src="vmu"> World ! <br>
 
 tt_draw(tt)
 
-print(tt.w, tt.h)
+--print(tt.w, tt.h)
 
