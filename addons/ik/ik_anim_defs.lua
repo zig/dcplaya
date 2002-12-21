bcb94f7aeceffcacaa7eb562c84ec39af781b944
ben <ben>
2002-12-21 09:28:23 +0000

    Some more file for IK remix.

---
+++ addons/ik/ik_anim_defs.lua
@@ -0,0 +1,130 @@
+--- @ingroup dcplaya_addons_ik
+--- @file    ik_anim_defs.lua
+--- @date    2002/12/19
+--- @author  benjamin gerard <ben@sashipa.com>
+--- @brief   Internationnal Karate Remix (animation definitions)
+--
+
+return {
+   ["walk"] = {
+      { "kamae0", 1 },
+      { "kamae1", 1 },
+      { "kamae2", 1 },
+      { "kamae1", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["mpunch"] = {
+      { "kamae0", 1 },
+      { "mpunch0", 1 },
+      { "mpunch1", 1 },
+      { "mpunch0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["hpunch"] = {
+      { "kamae0", 1 },
+      { "hpunch0", 1 },
+      { "hpunch1", 1 },
+      { "hpunch0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["hkick"] = {
+      { "kamae0", 1 },
+      { "kick0", 1 },
+      { "kick1", 1 },
+      { "hkick3", 1 },
+      { "kick1", 1 },
+      { "kick0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["mkick"] = {
+      { "kamae0", 1 },
+      { "kick0", 1 },
+      { "kick1", 1 },
+      { "mkick3", 1 },
+      { "kick1", 1 },
+      { "kick0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["lkick"] = {
+      { "kamae0", 1 },
+      { "kick0", 1 },
+      { "kick1", 1 },
+      { "lkick3", 1 },
+      --		 { "kick1", 1 },
+      { "kick0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["backkick"] = {
+      { "kamae0", 1 },
+      { "back0", 1 },
+      { "back1", 1 },
+      { "back2", 1 },
+      { "back3", 1 },
+      { "back4", 1 },
+      { "kick1", 1 },
+      { "kick0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["balayage"] = {
+      { "kamae0", 1 },
+      { "balayage0", 1 },
+      { "balayage1", 1 },
+      { "balayage0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["dancing"] = {
+      { "kamae0", 1 },
+      { "dancing0", 1 },
+      { "dancing1", 1 },
+      { "dancing2", 1 },
+      { "dancing3", 1 },
+      { "dancing2", 1 },
+      { "dancing1", 1 },
+      { "dancing2", 1 },
+      { "dancing3", 1 },
+      { "dancing2", 1 },
+      { "dancing1", 1 },
+      { "dancing0", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["jump"] = {
+      { "kamae0", 1 },
+      { "jump0", 1 },
+      { "jump1", 1 },
+      { "jump2", 1 },
+      { "jump2", 1 },
+      { "jump1", 1 },
+      { "kamae0", 1 },
+   },
+
+   ["coconut"] = {
+      { "kamae0", 1 },
+      { "coconut0", 1 },
+      { "coconut1", 1 },
+      { "coconut2", 1 },
+      { "coconut3", 1 },
+      { "coconut3", 1 },
+      { "coconut3", 1 },
+   },
+
+   ["salut"] = {
+      { "kamae0", 1 },
+      { "salut0", 1 },
+      { "salut1", 1 },
+      { "salut2", 1 },
+      { "salut1", 1 },
+      { "salut0", 1 },
+      { "kamae0", 1 },
+   },
+
+
+}
