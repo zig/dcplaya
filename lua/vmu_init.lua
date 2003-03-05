927fa6a00db7e38b1645f7febc7d720b6af0976d
ben <ben>
2003-03-05 20:02:18 +0000

    fix missing ')'

--- lua/vmu_init.lua
+++ lua/vmu_init.lua
@@ -1,7 +1,7 @@
 --- @date 2002/12/06
 --- @author benjamin gerard <ben@sashipa.com>
 --- @brief  LUA script to initialize dcplaya VMU backup.
---- $Id: vmu_init.lua,v 1.11 2003-03-05 17:48:30 ben Exp $
+--- $Id: vmu_init.lua,v 1.12 2003-03-05 20:02:18 ben Exp $
 ---
 
 -- Unload library
@@ -228,4 +228,5 @@ function vmu_init()
 end
 
 vmu_init()
-printf("VMU path [" .. tostring(vmu_path .. "]"
+printf("VMU path [" .. tostring(vmu_path .. "]")
+
