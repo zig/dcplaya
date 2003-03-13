72d0689489d4d37b717e0088f659916c58128a88
ben <ben>
2003-03-13 23:11:43 +0000

    Modify the vmu_init script. It could now be launched only for default file selection insteed of loading the selected file. A parameter allow to force the vmu choice even if there is only one file available. Add some global variables to allow vmu writing without confirmation and to desactive the default vmu file auto selection. Now the default file is handled by the C API (vmu_file.c) for a better synchronization between lua and C.

--- lua/vmu_init.lua
+++ lua/vmu_init.lua
@@ -1,30 +1,25 @@
 --- @date 2002/12/06
 --- @author benjamin gerard <ben@sashipa.com>
 --- @brief  LUA script to initialize dcplaya VMU backup.
---- $Id: vmu_init.lua,v 1.20 2003-03-11 15:18:06 ben Exp $
+--- $Id: vmu_init.lua,v 1.21 2003-03-13 23:11:43 ben Exp $
 ---
 
 -- Unload library
 vmu_init_loaded = nil
 
 --- Global VMU path variable
-vmu_path = nil
-vmu_leaf = nil
 ram_path = ram_path or nil
 
 --- Get current VMU file.
-function vmu_file(path,leaf)
-   path = path or vmu_path
-   leaf = leaf or vmu_leaf
-   return type(path) == "string"
-      and type(leaf) == "string"
-      and canonical_path(path .. '/' .. leaf)
+function vmu_file()
+   return type(vmu_file_default) == "function"
+      and vmu_file_default()
 end
 
 --- Set current VMU file.
 function vmu_set_file(filepath)
-   vmu_path,vmu_leaf = get_path_and_leaf(filepath)
-   return vmu_file()
+   return type(vmu_file_default) == "function"
+      and vmu_file_default(filepath)
 end
 
 function ramdisk_init(path)
@@ -48,6 +43,7 @@ end
 -- Load required libraries
 if not dolib("vmu_select") then return nil end
 if not dolib("textviewer") then return nil end
+if not dolib("fileselector") then return nil end
 
 --- Initialize the RAM disk.
 --
@@ -108,10 +104,6 @@ function vmu_load_file(fname,path)
       printf("vmu_load_file [%s] : failed", fname)
    end
 end
-
-function dcplaya_welcome()
-end
-
 
 --- Get pluged VMU list.
 function vmu_list()
@@ -186,113 +178,158 @@ function vmu_choose_file(files, dir, expr)
    else
       choice = evt_run_standalone(vs)
       if type(choice) == "string" then
-	 return vmu_set_file("/vmu/" .. choice)
+	 return "/vmu/" .. choice
       end
+   end
+end
+
+
+function welcome(standalone)
+   -- Read info
+   -- $$$ todo : add localisation folder...
+   local rscpath = home .. "lua/rsc/text/"
+   local newbie_text = rscpath .. "newbie.txt"
+   local introduction_text = rscpath .. "introduction.txt"
+   local plugins_text = rscpath .. "plugins.txt"
+   local welcome_text = rscpath .. "welcome.txt"
+   local warning_text = rscpath .. "warning.txt"
+   local greetings_text = rscpath .. "greetings.txt"
+   local authors_text = rscpath .. "authors.txt"
+   local tv =
+      gui_text_viewer(nil,
+		      {
+			 newbie    = newbie_text,
+			 welcome   = welcome_text,
+			 warning   = warning_text,
+			 plugins   = plugins_text,
+			 greetings = greetings_text,
+			 authors   = authors_text,
+		      } , nil, "Welcome", nil)
+   if (tv) then
+      gui_text_viewer_set_tt(tv,"welcome");
+      if not standalone then
+	 return tv
+      end
+      evt_run_standalone(tv);
    end
 end
 
 --- Initialise VMU path.
 --
-function vmu_init(force_choice)
-   local done
-   local choice = nil
+--- @param  not_load      if set choosen file will be setted as default but
+---                       not loaded.
+--- @param  force_choice  If set force to choose a vmu unit and file.
+function vmu_init(not_load, force_choice)
 
-   while not done do
-      local found = vmu_find_files()
+   force_choice = (force_choice ~= nil) or 0 -- force to 0/1
+
+   -- Infinite looooooopzzzz !!
+   while not nil do
+      local choice = nil
+      local found = force_choice == 0 and vmu_find_files()
+      local count = (found and found.n) or 0
 
       --- Must select a file if more than one was found
-      if found and found.n and found.n > 1 then
+      if count > 1 then
 	 local result
 	 result = gui_ask('More than one dcplaya save file has been found. You are going to select the one you want to use.',
 			  { "select file","cancel" },
 			  nil,
 			  "More than one dcplaya file")
 	 if result and result == 1 then
+	    -- User don't want to choose ! Deletes the found !
+	    count = 0
+	    choice = nil 
+	 else
 	    choice = vmu_choose_file(found)
-	 else
-	    choice = vmu_set_file(nil)
-	    return
-	 end
-      else
-	 if not force_choice and found and found.n and found.n == 1 then
-	    choice = "/vmu/" .. found[1].path .. "/" .. found[1].name
-	    printf("CHOICE UNIC %q",choice)
-	 else
-	    local ok
-	    while not ok do
-	       local result = gui_ask('<left>No dcplaya save has been file found.<p vspace="2">It may be the first time you launch dcplaya. If it is the case, you should read dcplaya information.<p><vspace h="20"><center><font size="20" color="#FFFF00">First aid<p vspace="4"><vspace h="10"><font size="16" color="#FFFFFF">\016 confirm<br>\017 cancel<br><font size="8">\004\006<font size="16"> move focus cursor<br><br>',
-				      {'read info', 'create save', 'cancel'},
-				      440,
-				      "No dcplaya save file"
-				)
-	       result = result or 0
-	       if result == 1 then
-		  -- Read info
-		  -- $$$ todo : add localisation folder...
-		  local rscpath = home .. "lua/rsc/text/"
-		  local newbie_text = rscpath .. "newbie.txt"
-		  local introduction_text = rscpath .. "introduction.txt"
-		  local plugins_text = rscpath .. "plugins.txt"
-		  local welcome_text = rscpath .. "welcome.txt"
-		  local warning_text = rscpath .. "warning.txt"
-		  local greetings_text = rscpath .. "greetings.txt"
-		  local authors_text = rscpath .. "authors.txt"
-		  local tv = gui_text_viewer(nil,
-					     {
-						newbie    = newbie_text,
-						welcome   = welcome_text,
-						warning   = warning_text,
-						plugins   = plugins_text,
-						greetings = greetings_text,
-						authors   = authors_text,
-					     } , nil, "Welcome", nil)
-		  if (tv) then
-		     gui_text_viewer_set_tt(tv,"welcome");
-		     evt_run_standalone(tv);
-		  end
-		  tv = nil
-	       elseif result == 2 then
-		  ok = 1
-	       else
-		  return
-	       end
+	    if not choice then
+	       -- User don't makes a choice, delete the found and continue. May
+	       -- be he wants to choose another the in another vmu.
+	       count = 0
 	    end
 	 end
       end
-      
+
+      if count == 1 then
+	 -- found one file only and don'y force choice :
+	 -- just get  it and continue.
+	 choice = "/vmu/" .. found[1].path .. "/" .. found[1].name
+      end
+
+      -- If we don't has a choice here, we had to choose a vmu. If the
+      -- default vmu is not setted, we assume is is a new user and display
+      -- a welcome dialog.
       if not choice then
-	 choice = vmu_choose()
-	 if choice then
-	    choice = vmu_set_file(choice .. "/dcplaya.01")
-	 end
-      else
-	 done = vmu_load_file(choice, "/ram/dcplaya")
-	 if not done then
-	    choice = vmu_set_file(nil)
-	    local result
-	    result = gui_ask('<left>Invalid dcplaya save file. File may be corrupt.<br><center><img name="grimley" src="stock_grimley.tga" scale="1.5">',
-			     { "try another","cancel" },
-			     nil,
-			     "vmu file error")
-	    if not result or result ~= 1 then
+	 local ok = (force_choice ~= 2) and (vmu_file_default() ~= nil)
+
+	 while not ok do
+	    local result =
+	       gui_ask('<left>No dcplaya save has been file found.<p vspace="2">It may be the first time you launch dcplaya. If it is the case, you should read dcplaya information.<p><vspace h="20"><center><font size="20" color="#FFFF00">First aid<p vspace="4"><vspace h="10"><font size="16" color="#FFFFFF">\016 ... confirm<br>\017 ... cancel<br><font size="8">\004.\006<font size="16"> ... move focus cursor<br><br>',
+		       {'read info', 'create save', 'cancel'},
+		       440,
+		       "No dcplaya save file"
+		    )
+	    result = result or 0
+	    if result == 1 then
+	       -- Read info choosen
+	       welcome(1)
+	    elseif result == 2 then
+	       -- Create save file choose.
+	       ok = 1 -- exit loop
+	    else
+	       -- cancel choosen. Exit with no further modification.
 	       return
 	    end
-
-	    -- avoid looping
-	    force_choice = found and found.n and found.n == 1
-
 	 end
+
+	 -- Select our vmu unit
+	 choice = vmu_choose()
+	 if choice then
+	    -- Now we have choosen a vmu, we must choose a file
+	    local fs = fileselector("Choose VMU file",choice,"dcplaya.01")
+	    choice = fs and evt_run_standalone(fs)
+	 end
+
+	 if not choice then
+	    force_choice = 2 -- No choice made here, Next loop display welcome.
+	 end
+
+      end
+
+      if choice then
+	 -- Now we have a choice, try to do it !
+	 local done
+	 if not_load then
+	    done = vmu_set_file(choice)
+	 else
+	    done = vmu_load_file(choice)
+	 end
+	 if done then
+	    -- Everything is OK. Bye-Bye !
+	    return 1
+	 end
+	 printf("[vmu_init] : %q not a valid vmu file.",choice)
+	 result =
+	    gui_ask('<left>Invalid dcplaya save file. File may be corrupt.<br><center><img name="grimley" src="stock_grimley.tga" w="40">',
+		    { "try another","cancel"},
+		    nil,
+		    "vmu file error")
+	 if not result or result ~= 1 then
+	    return
+	 end
+	 choice = nil
+	 force_choice = 1 -- After one failure, we force choice !
       end
    end
-   printf("VMU [ path=%q leaf=%q path=%q ]",
-	  tostring(vmu_path),tostring(vmu_leaf),
-	  tostring(vmu_file()))
 end
 
 function vmu_confirm_write(path)
    local r
    if type(path) ~= "string" then
       return
+   end
+   if vmu_never_confirm_write then
+      return 1
    end
 
    local col = color_tostring(gui_text_color) or "#FFFFB0"
