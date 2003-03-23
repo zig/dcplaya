16c8768e65dd547ab449260367eeb24b16bdfa63
ben <ben>
2003-03-23 00:48:55 +0000

    Script to create a doxygen file from a default one and changing some parameters.

---
+++ utils/doxygeneric.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+# 
+# (C) 2003 benjamin gerard <ben@sashipa.com>
+#
+# Generate a generic doxygen configuration file
+#
+
+function transform
+{
+    name="`expr "$1" : '\(.*\)=.*' \| "$1"`"
+    val="`expr "$1" : '.*=\(.*\)' \| ""`"
+    echo "s¬${name}\([[:space:]]*\)=.*¬${name}\1= ${val}¬"
+}
+
+export line=
+
+file="$1"
+shift
+i=0;
+while [ $# -ne 0 ]; do
+    case "$1" in
+	-D)
+	    line[$i]="-e"
+	    ;;
+	*)
+	    line[$i]="`transform "$1"`"
+	    ;;
+    esac
+    shift
+    i=`expr $i + 1`
+done
+echo "output=$file"
+echo "i=$i" 1>&2
+echo "${line[@]}" 1>&2
+sed  "${line[@]}" > "${file}" || rm -f "${file}"
+echo result=$?
